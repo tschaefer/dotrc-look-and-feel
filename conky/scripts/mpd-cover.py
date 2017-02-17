7c82bdd2b8727013b0aade44a1ebb3fb53a80a9a
tschaefer <github@blackox.org>
2017-02-17 01:05:58 +0100

    conky: convert config to new syntax v1.10.x; modified scripts

--- conky/scripts/mpd-cover.py
+++ conky/scripts/mpd-cover.py
@@ -14,21 +14,21 @@ song_file = song_info['file']
 
 # get cover
 image_extensions = (".jpg", ".jpeg", ".png")
-music_library = '/mnt/berta/media/music'
+music_library = '/mnt/smb/media/music'
 song_path = os.path.join(music_library, song_file)
 if os.path.exists(song_path):
-	song_dir = os.path.dirname(song_path)
-	for f in os.listdir(song_dir):
-		if f.endswith(image_extensions):
-			song_cover = f
-			break
+    song_dir = os.path.dirname(song_path)
+    for f in os.listdir(song_dir):
+        if f.endswith(image_extensions):
+            song_cover = f
+            break
 
 # set symlink
 try:
-	os.unlink('/tmp/mpd.cover')
+    os.unlink('/tmp/mpd.cover')
 except:
-	pass
+    pass
 if 'song_cover' not in vars():
-	os.symlink(os.path.join(os.environ['HOME'], '.conky/scripts/mpd-no-cover.jpg'), '/tmp/mpd.cover')
+    os.symlink(os.path.join(os.environ['HOME'], '.conky/scripts/mpd-no-cover.jpg'), '/tmp/mpd.cover')
 else:
-	os.symlink(os.path.join(song_dir, song_cover), '/tmp/mpd.cover')
+    os.symlink(os.path.join(song_dir, song_cover), '/tmp/mpd.cover')
